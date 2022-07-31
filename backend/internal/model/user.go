b69dfdfbb4afdfa9cc32f05ca22c46a6f1722435
tajale72 <55036881+tajale72@users.noreply.github.com>
2022-07-31 13:24:51 -0500

    Delete backend directory

--- backend/internal/model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-//User is a struct
-type User struct {
-	Name   string   `json:"name"`
-	Age    int      `json:"age"`
-	Arr    []string `json:"arr"`
-	Origin string   `json:origin`
-}
