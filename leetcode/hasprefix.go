e104d20cb26507e235797d3776491386a02ba0ca
Tajale72 <Tajale72@gmail.com>
2023-03-08 19:09:19 -0600

    leetcode practices

---
+++ leetcode/hasprefix.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+		for !strings.HasPrefix(strs[i], prefix) {
+			prefix = prefix[:len(prefix)-1]
+			if prefix == "" {
+				return ""
+			}
+		}
+	}
+	return prefix
+}
+
+func main() {
+	strs := []string{"flower", "flow", "flight"}
+	fmt.Println(longestCommonPrefix(strs))
+
+}
